7d24593eeb07ccffbfe3bb964462ac842c4d5a27
Abhishek_Chaturvedi <githubavishekchaturvedi0@gmail.com>
2025-01-03 15:20:35 +0530

    updates

--- controllers/home-Controllers.js
+++ controllers/home-Controllers.js
@@ -3,55 +3,55 @@ const recentUpdates = (req, res) => {
         const updates = {
             mainUpdates: [
                 {
-                    date: "2nd December 2024",
-                    title: "VSV Builders Second Quarter Updates",
-                    description: "Comprehensive overview of our recent construction projects and innovative approaches to building sustainable infrastructure.",
-                    image: "https://plus.unsplash.com/premium_photo-1682617326364-8fb636f7bea0?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    date: "January 5, 2025",
+                    title: "Pioneering Telecom Excellence",
+                    description: "With 19+ years of experience, VS&V delivers innovative telecom solutions across India and Nepal, trusted by global leaders like Huawei and Smarttel.",
+                    image: "https://media.licdn.com/dms/image/v2/D5622AQGPfbqYP0JLVw/feedshare-shrink_2048_1536/B56ZQpIoqoG8As-/0/1735856918218?e=1738800000&v=beta&t=s5vr5Locc7q51PIyf31mwQoG_z-JNG1u2YxfDmLjmco",
                     isFirstRow: true
                 },
                 {
-                    date: "15th November 2024",
-                    title: "Innovative Construction Technologies",
-                    image: "https://images.unsplash.com/photo-1535732759880-bbd5c7265e3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
-                    description: "Exploring cutting-edge technologies",
+                    date: "January 8, 2025",
+                    title: "Innovating at New Heights",
+                    description: "VS&V creates functional and inspiring interiors, setting new standards in design and craftsmanship.",
+                    image:"https://media.licdn.com/dms/image/v2/D5622AQEL_FK8PkM8ww/feedshare-shrink_1280/B56ZQpIoq3HoAk-/0/1735856917964?e=1738800000&v=beta&t=ykfUo_SjJ1CBCRqAlx29bNxEvVZC2yp9Ely_IA833AM",
                     isBlack: true
                 },
                 {
-                    date: "10th October 2024",
+                    date: "January 12, 2025",
                     title: "Sustainability in Modern Construction",
-                    image: "https://images.unsplash.com/photo-1535732759880-bbd5c7265e3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
-                    description: "Our commitment to environmentally friendly building practices"
+                    image: "https://media.licdn.com/dms/image/v2/D5622AQGxod-Y1pmOnA/feedshare-shrink_2048_1536/B56ZQpIop9GsAo-/0/1735856918314?e=1738800000&v=beta&t=azl96EvrL9YGLz9RF1TG8s7uLJ7eJYfDMu0BMSTDuR4",
+                    description: "VS&V excels in challenging terrains, exemplified by projects like Mount Gauri Shankar Base."
                 }
             ],
             bottomCards: [
                 {
                     type: "About",
-                    date: "5th September 2024",
-                    title: "VSV Communication Milestone",
-                    image: "https://images.unsplash.com/photo-1535732759880-bbd5c7265e3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
-                    description: "Celebrating our achievements and the impact we've made in the construction and communication sectors."
+                    date: "January 15, 2025",
+                    title: "Fostering Team Spirit",
+                    image: "https://media.licdn.com/dms/image/v2/D5622AQG_0joR4-CW8g/feedshare-shrink_2048_1536/B56ZQpIoqqHIAs-/0/1735856918153?e=1738800000&v=beta&t=heXD2cFJjvZ5NC3CgCFG343Et5M-dxF6s9xy8OFlSdg",
+                    description: "VS&V promotes teamwork and morale through events like our Annual Meeting, building a culture of excellence."
                 },
                 {
                     type: "Services",
-                    date: "20th August 2024",
-                    title: "Comprehensive Construction Services",
-                    image: "https://images.unsplash.com/photo-1626885930974-4b69aa21bbf9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
-                    description: "A deep dive into the range of services we offer, from project planning to final execution.",
+                    date: "January 20, 2025",
+                    title: "Building the Future",
+                    image: "https://media.licdn.com/dms/image/v2/D5622AQHjRrlCv_dMwQ/feedshare-shrink_2048_1536/B56ZQpIoqrGQAo-/0/1735856918401?e=1738800000&v=beta&t=gr4yunr89XMZqPw2rbbdQtD-gPDQpwZeiDKQt4-_IOw",
+                    description: "VS&V is shaping skylines with innovative construction and telecom infrastructure projects.",
                     isBlack: true
                 },
                 {
                     type: "Post",
-                    date: "1st July 2024",
-                    title: "Urban Development Insights",
-                    image: "https://images.unsplash.com/photo-1626885930974-4b69aa21bbf9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
-                    description: "Analyzing the latest trends and challenges in urban construction and infrastructure development."
+                    date: "January 25, 2025",
+                    title: "Certified for Excellence",
+                    image: "https://media.licdn.com/dms/image/v2/D5622AQGSAPXuMBjPQQ/feedshare-shrink_2048_1536/B56ZQpIoq9HIA0-/0/1735856918007?e=1738800000&v=beta&t=LOS-Banvyr2DJ8EWj3DD9Fts-dqIDe502EHsY1iDNx0",
+                    description: "Our ISO certifications reflect our dedication to quality and safety in every project."
                 },
                 {
                     type: "Projects",
-                    date: "15th June 2024",
-                    title: "Landmark Project Showcase",
-                    image: "https://images.unsplash.com/photo-1626885930974-4b69aa21bbf9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
-                    description: "Highlighting our most significant and impactful construction projects of the year.",
+                    date: "January 30, 2025",
+                    title: "Introducing the VS&V Group",
+                    image: "https://media.licdn.com/dms/image/v2/D5622AQFCSN7GoXBWSw/feedshare-shrink_2048_1536/B56ZQpIoqhGQAo-/0/1735856920140?e=1738800000&v=beta&t=McBl0o3Heg9RXig0kf2KtlXh_ReayHtf4qfk6BFJz8A",
+                    description: "VS&V Group combines expertise from Communication and Techno Engineering for greater impact and growth.",
                     isBlack: true
                 }
             ]
